9a3e47bef576fb21b19517fe154abe33e48e5d8b
Lukasz Magdziarz <lukasz_magdziarz@trimble.com>
2018-09-07 11:06:04 +0200

    Add Hello World application

---
+++ HellloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HellloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
