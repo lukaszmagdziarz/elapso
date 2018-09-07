dba812b963beebab0b8d0fe07a9af8be9546697b
lukaszmagdziarz <lukasz.magdziarz@onet.eu>
2018-09-07 19:15:47 +0200

    Add Tests project

--- HellloWorldApp/Program.cs
+++ HellloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HellloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Write something more...");
         }
     }
 }
