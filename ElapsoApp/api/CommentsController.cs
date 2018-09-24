cc878104a7a2b41f056c2e0ad09babed8ca4681f
lukaszmagdziarz <lukasz.magdziarz@onet.eu>
2018-09-24 12:01:20 +0200

    Add delete comment feature

---
+++ ElapsoApp/api/CommentsController.cs
@@ -0,0 +1,37 @@
+﻿using ElapsoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElapsoApp.api
+{
+    public class CommentsController : ApiController
+    {
+        private ElapsoContext db = new ElapsoContext();
+
+        // GET api/<controller>
+        public IEnumerable<Comment> Get(int counterId)
+        {
+            var comments = db.Comments.Where(c => c.CounterId==counterId);
+            return comments.ToList();
+        }
+
+        // PUT api/<controller>
+        public void Put([FromBody] Comment comment)
+        {
+            db.Comments.Add(comment);
+            db.SaveChanges();
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+        }
+    }
+}
